83066db547e2fd8050529fa648930933633d0c10
Riley Tatum <tatumr@uci.edu>
2017-06-21 11:54:58 -0700

    Added Javascript to handle menus and password comparing.

--- Prello_board.js
+++ Prello_board.js
@@ -1,21 +1,34 @@
-var main = function() {
-  $('#board-list-but').click(function() {
-    $('#board-list').toggle();
-  });
+var opt_menu_btn = document.querySelector("#opt-menu-btn");
+var opt_menu = document.querySelector("#opt-menu");
+var board_list_btn = document.querySelector("#board-list-btn");
+var board_list = document.querySelector("#board-list");
+var menu_btn = document.querySelector("#menu-open");
+var menu = document.querySelector("#menu");
+var close_menu = document.querySelector("#close");
 
-  $('#menu-open').click(function() {
-    $('#menu').animate({
-      width: "300px"
-    },
-    200);
-  });
+opt_menu_btn.addEventListener("click", function() {
+  if(opt_menu.style.display === "none") {
+    opt_menu.style.display = "block";
+  }
+  else {
+    opt_menu.style.display = "none";
+  }
+});
 
-  $('#close').click(function() {
-    $('#menu').animate({
-      width: "0"
-    },
-    200);
-  });
-};
+board_list_btn.addEventListener("click", function() {
+  if(board_list.style.display === "none") {
+    board_list.style.display = "block";
+  }
+  else {
+    board_list.style.display = "none";
+  }
+});
 
-$(document).ready(main);
+menu_btn.addEventListener("click", function() {
+  menu.style.width = "300px";
+});
+
+close_menu.addEventListener("click", function() {
+  menu.style.width = "0";
+  menu.style.transition = "all .2s"
+})
