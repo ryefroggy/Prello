73d798be993b148be4847503060510b878478588
Riley Tatum <tatumr@uci.edu>
2017-06-23 00:28:53 -0700

    Can now add cards.

--- Prello_board.js
+++ Prello_board.js
@@ -9,6 +9,7 @@ var add_cards = document.querySelectorAll(".add-card");
 var modal = document.querySelector("#modal");
 var modal_bg = document.querySelector("#modal-bg");
 var modal_close = document.querySelector("#close-card");
+var card_title_form = document.querySelector("#card-head-form");
 
 // webpage data structure
 function list(name) {
@@ -23,6 +24,12 @@ function card(title) {
 }
 
 var data = [];
+
+data.push(new list("First"));
+data.push(new list("Second"));
+data.push(new list("Third"));
+data.push(new list("Fourth"));
+
 
 
 opt_menu_btn.addEventListener("click", function() {
@@ -55,8 +62,8 @@ close_menu.addEventListener("click", function() {
 // Populate New Card card
 for(var i = 0; i < add_cards.length; i++) {
   add_cards[i].addEventListener("click", function() {
-      var card = this.parentNode;
-      var title = card.firstChild;
+      var parent = this.parentNode;
+      var title = parent.firstChild;
       var form = document.querySelector("#card-head-form");
       var left_card = document.querySelector("#card-left");
       var left_list = document.querySelector("#left-list");
@@ -79,4 +86,33 @@ modal_close.addEventListener("click", function() {
   var left = document.querySelector("#card-left");
   var list_title = left.querySelector("p");
   left.removeChild(list_title);
+});
+
+card_title_form.addEventListener("submit", function(f) {
+  f.preventDefault();
+  var card_title = document.querySelector("#card-title");
+  var title = card_title.value;
+  var parent = card_title_form.parentNode;
+  var list_str = parent.querySelector("p");
+  var list_name = list_str.textContent.substr(8);
+  for (var i = 0; i < data.length; i++) {
+    if(list_name === data[i].name) {
+      data[i].cards.push(new card(title));
+    }
+  }
+  card_title_form.style.display = "none";
+  var title_h = document.createElement("h3");
+  var title_p = document.createElement("p");
+  title_h.textContent = title;
+  title_p.textContent = title;
+  parent.insertBefore(title_h, list_str);
+  var li = document.createElement("li");
+  for (var i = 0; i < add_cards.length; ++i) {
+    var a_parent = add_cards[i].parentNode;
+    var title = a_parent.firstChild;
+    if(list_name === title.textContent) {
+      title.nextElementSibling.appendChild(li);
+      li.appendChild(title_p);
+    }
+  }
 });
