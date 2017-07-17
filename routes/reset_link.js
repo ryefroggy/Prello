3a5f772ac22caf05b6161fc7f99d5f1cdeea952a
Riley Tatum <tatumr@uci.edu>
2017-07-17 13:23:22 -0700

    added Not found for accessing old link

--- routes/reset_link.js
+++ routes/reset_link.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var User_reset = require('../models/user_resets');
 
 /* GET home page. */
 router.get('/:HASH', function(req, res, next) {
-  res.render('reset_link', { title: 'Reset Link' , path: '/stylesheets/reset_link.css', js_path: "", hash:"http://localhost:3000/forgot_password/"+req.params.HASH});
+  User_reset.findOne({hash: req.params.HASH}, function(err, user) {
+    if(user) {
+      res.render('reset_link', { title: 'Reset Link' , path: '/stylesheets/reset_link.css', js_path: "", hash:"http://localhost:3000/forgot_password/"+req.params.HASH});
+    }
+    else {
+      res.status(404).send("Not found.");
+    }
+  });
 });
 
 module.exports = router;
